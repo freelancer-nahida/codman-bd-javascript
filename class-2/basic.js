cde148a771e0781d4c5e64e91fd6b4bb28bb9438
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-06-23 09:21:09 +0600

    add variable.js

---
+++ class-2/basic.js
@@ -0,0 +1,57 @@
+
+        // variable
+
+        // first we should define a variable with var
+        //first letter should be smaller
+        // camelCase /underscore
+
+        // var number_two =20;  // under_score
+        var numOne = 10;//camelCas
+        var numTwo = 40;
+        // operator 
+
+        var addition = numOne + numTwo;
+        var division = numOne / numTwo;
+        var multiply = numOne * numTwo;
+        var subtract = numOne - numTwo;
+
+        //adding to the console
+        //Result
+        console.log('addition:', addition);
+        console.log('division :', division);
+        console.log('multiply:', multiply);
+        console.log('subtract:', subtract);
+
+        // console.log(100 ** 2);//exponentition
+        // console.log(numOne % numTwo);//modulus
+
+        // var numIncrement = 10;
+        // numIncrement++;//increment
+        // numIncrement++;//decrement
+        // numIncrement++;
+        // numIncrement--;
+        // numIncrement--;
+        // numIncrement--;
+        // numIncrement--;
+        // numIncrement--;
+
+        // console.log(numIncrement);
+
+        //boolean
+
+        // var isRaining = true; // yes
+
+        // var isDarkMode = false; // or no
+
+        // console.log(typeof isRaining); //boolean
+
+
+      var  counter: // undifined kono value daya nai
+
+
+      console.log(counter);
+
+      //null// khali
+
+      var number = null;
+     
---
+++ class-2/variable.js
@@ -0,0 +1,55 @@
+// console.log('variable');
+var boxOne = 'orange';
+var boxTwo = 'orange';
+
+var personName, age, job, student;
+
+personName = "Sujon Wp";
+
+age = 25;
+job = 'Devloper';
+
+student = 'univercity  of codmanbd '
+
+console.log(personName, age, job, student);
+
+age = 30;
+age = 50;
+age = 90;
+console.log(age);
+
+// ES6+ latest varsion
+//let & const
+
+userName = " Mamun";
+
+var userName = 'sujan';
+
+
+// userName = 'asasds';
+// let userName;
+
+console.log(userName);
+
+//let
+//global variable
+// let carModel = 'nhk';
+// {
+//     let carModel = 'bmw';//local variable
+//     console.log(carModel);
+// }
+// console.log(carModel);
+
+// var carModel = 'nhk';
+// {
+//     var carModel = 'bmw';//local variable
+//     console.log(carModel);
+// }
+// console.log(carModel);
+
+
+const userName = 'Amit';
+console.log(userName);
+
+userName = 'sujon';
+console.log(userName);
