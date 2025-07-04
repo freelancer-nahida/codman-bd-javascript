5169e96e5176ea177a6ec1fb5c8ac334ca35bd94
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-07-04 10:19:19 +0600

    add arrary.js

---
+++ array.js
@@ -0,0 +1,23 @@
+// Array
+
+let fruits = ['apple', 'banana', 'lichi', 'mango', 5, 9, 6, 7, true];
+
+// console.log(fruits);
+// console.log(fruits[6]);
+// console.log(fruits.length);
+
+// console.log(fruits.indexOf('apple'));
+
+fruits.pop(); // remove last item
+console.log(fruits);
+fruits.shift();//remove the first item an array
+console.log(fruits);
+
+fruits.push('berri');
+console.log(fruits);// add item to the array  at the end
+
+fruits.unshift('blackbari');// add item to the first
+
+console.log(fruits);
+
+
