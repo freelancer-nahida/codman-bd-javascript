b7c45bd4d81420b04b73f2851670c68c83b3196b
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-07-04 23:28:12 +0600

    add function.js

--- array.js
+++ array.js
@@ -1,23 +1,55 @@
-// Array
+// // Array
 
-let fruits = ['apple', 'banana', 'lichi', 'mango', 5, 9, 6, 7, true];
+// let fruits = ['apple', 'banana', 'lichi', 'mango', 5, 9, 6, 7, true];
+
+// // console.log(fruits);
+// // console.log(fruits[6]);
+// // console.log(fruits.length);
+
+// // console.log(fruits.indexOf('apple'));
+
+// fruits.pop(); // remove last item
+// console.log(fruits);
+// fruits.shift();//remove the first item an array
+// console.log(fruits);
+
+// fruits.push('berri');
+// console.log(fruits);// add item to the array  at the end
+
+// fruits.unshift('blackbari');// add item to the first
 
 // console.log(fruits);
-// console.log(fruits[6]);
-// console.log(fruits.length);
 
-// console.log(fruits.indexOf('apple'));
+//joining an array
 
-fruits.pop(); // remove last item
-console.log(fruits);
-fruits.shift();//remove the first item an array
-console.log(fruits);
+// let arr1 = ['am', 'jam']
+// let arr2 = ['banana']
 
-fruits.push('berri');
-console.log(fruits);// add item to the array  at the end
 
-fruits.unshift('blackbari');// add item to the first
+// let newArr = arr1.concat(arr2);
+// console.log(newArr);
 
-console.log(fruits);
+//slicing an array 
+
+// let animal = ['tiger', 'snake', 'singho']
+
+// let newAnimal = animal.slice(0, 2)
+// console.log(newAnimal);
+
+//splice (add/ remove)- change the orginal artray
+
+// start , deleete,item...
+let animal = ['tiger', 'snake', 'singho', 1, 2, 5, 8, 7, 22, 99, 22, 66, 44, 55, 77]
+
+// let newAnimal = animal.splice(2, 1, 'dog')
+// console.log(animal);
+
+//sorting
+
+let animalSort = animal.sort((a, b) => a - b);
+console.log(animalSort.reverse());
+
+
+
 
 
